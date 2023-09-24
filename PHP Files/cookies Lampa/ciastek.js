2ea0e5d4cf9e8ae8b69c7920ef0977f142bc2ffc
ElPollaco <elpollaco.official@gmail.com>
2023-09-25 00:56:51 +0200

    cleaning

--- PHP Files/cookies Lampa/ciastek.js
+++
@@ -1,8 +0,0 @@
-const wyn = document.querySelector("#wyn-js");
-const body = document.querySelector("body");
-
-body.addEventListener("click", () => {
-    const obiekt = document.activeElement.tagName;
-    wyn.innerHTML = obiekt;
-    document.cookie = `ciastek=${obiekt}`;
-});
